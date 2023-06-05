9f8c5f6bb213d37f60d496364c6e61c6b5899374
TRBogdann <tr.bogdan50@gmail.com>
2023-06-05 22:53:59 +0300

    site

--- site/src/protofolio.js
+++ site/src/protofolio.js
@@ -3,6 +3,9 @@ function Portofolio()
     return (
         <div className="smt">
             Portofolio
+            <div className="slideshow"></div>
+            <div className="slideshow2"></div>
+            <div className="slideshow3"></div>
         </div>
     );
 }
